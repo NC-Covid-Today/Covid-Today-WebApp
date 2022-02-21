2e78280207e6bba426f58195b0869b90df107ecb
Sam Prewitt <prewittsr@appstate.edu>
2022-02-21 17:57:33 -0500

    placed logo and title

--- src/App.js
+++ src/App.js
@@ -6,19 +6,23 @@ function App() {
   return (
     <div className="App">
       <title className="Site-header"> 
-        <p><img src={site_logo} className="Site-logo" alt="covidtoday_logo" /> </p>
-        NC Covid Today
-      </title>
+          <div class="aligned">
+            <img src={site_logo} className="Site-logo" alt="covidtoday_logo" /> 
+            <span>NC Covid Today</span>
+          </div> 
+      </title>      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
+        /*
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          */
         >
           Learn React
         </a>

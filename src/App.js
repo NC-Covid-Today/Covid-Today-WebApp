d551fcf1060a3bf27eb80bc0e79ac0ea9c5da5da
Sam Prewitt <prewittsr@appstate.edu>
2022-02-21 16:23:03 -0500

    placed logo and title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
+import site_logo from './covidtoday_logo.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <title className="Site-header"> 
+        <p><img src={site_logo} className="Site-logo" alt="covidtoday_logo" /> </p>
+        NC Covid Today
+      </title>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>

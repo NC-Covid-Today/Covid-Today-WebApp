3f7394a14252af5cae84c8c15b42a366a188d09b
SamPrewitt <prewittsr@appstate.edu>
2022-02-28 17:23:10 -0500

    test 01

--- src/App.js
+++ src/App.js
@@ -16,15 +16,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-        /*
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          */
-        >
-          Learn React
+        <a>
+          Tasks: Add a new page to web-app
         </a>
       </header>
     </div>

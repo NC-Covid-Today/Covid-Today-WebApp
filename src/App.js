37cdd54adc32403230b07582f91d90fc673f4446
SamPrewitt <prewittsr@appstate.edu>
2022-03-21 01:03:22 -0400

    NEED TO RUN WITH LAST VERSION OF NODE!! Only works on local machine in current set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import logo from './logo.svg';
 import site_logo from './covidtoday_logo.jpg';
+//import './App.css';
+
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route}
+    from 'react-router-dom';
+import Home from './pages';
+import About from './pages/about';
+import Blogs from './pages/blogs';
+import SignUp from './pages/signup';
+import Contact from './pages/contact';
 
 function App() {
   return (
+    /*
+    <Router>
+    <Navbar />
+    <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/blogs' element={<Blogs/>} />
+        <Route path='/sign-up' element={<SignUp/>} />
+    </Routes>
+    </Router>
+    );*/
     <div className="App">
       <title className="Site-header"> 
           <div class="aligned">
             <img src={site_logo} className="Site-logo" alt="covidtoday_logo" /> 
             <span>NC Covid Today</span>
           </div> 
-      </title>      
+      </title> 
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/blogs' element={<Blogs />} />
+          <Route path='/sign-up' element={<SignUp />} />
+        </Routes>
+      </Router>     
+    
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Covid Today is a web-app for distributing Covid19 information via data visualizations and news articles.  There will be visualizations on NC specific data, such as infection rate. Pages populated with relevant news articles and a viz for world data will also be included to share a "bigger picture" of the pandemic. 
+        
+          TODO: 
+            * Add multiple pages
+            * Embed Tableau Viz
         </p>
-        <a>
-          Tasks: Add a new page to web-app
-        </a>
+        
       </header>
+      
     </div>
   );
 }

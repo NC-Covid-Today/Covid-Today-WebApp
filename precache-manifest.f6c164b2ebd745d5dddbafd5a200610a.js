8ef8a6de819ec3b4732cea8be3c29644db7c5cf6
SamPrewitt <prewittsr@appstate.edu>
2022-03-28 13:10:01 -0400

    Updates

--- precache-manifest.f6c164b2ebd745d5dddbafd5a200610a.js
+++ precache-manifest.f6c164b2ebd745d5dddbafd5a200610a.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "782b20c42696cacf1d4d",
+    "revision": "aa22137d0be822f1be00",
     "url": "/Covid-Today-WebApp/static/css/main.758812a8.chunk.css"
   },
   {
-    "revision": "782b20c42696cacf1d4d",
-    "url": "/Covid-Today-WebApp/static/js/main.7bccd72d.chunk.js"
+    "revision": "aa22137d0be822f1be00",
+    "url": "/Covid-Today-WebApp/static/js/main.0b7d58a3.chunk.js"
   },
   {
     "revision": "705fdd67b87de4bbce74",
     "url": "/Covid-Today-WebApp/static/js/runtime~main.2499f985.js"
   },
   {
-    "revision": "f02aa2a8bb176324a610",
-    "url": "/Covid-Today-WebApp/static/js/2.9f39a60e.chunk.js"
+    "revision": "9017aea749b68e321ae7",
+    "url": "/Covid-Today-WebApp/static/js/2.b08ce614.chunk.js"
   },
   {
     "revision": "d1b1c15550733d4c89ea3d2b1bb1ea5c",
     "url": "/Covid-Today-WebApp/static/media/covidtoday_logo.d1b1c155.jpg"
   },
   {
-    "revision": "22a7de068e6eb52d4ea1ee7569328a13",
+    "revision": "88aef27bef2323f3a4e303c7f8079ba3",
     "url": "/Covid-Today-WebApp/index.html"
   }
 ];

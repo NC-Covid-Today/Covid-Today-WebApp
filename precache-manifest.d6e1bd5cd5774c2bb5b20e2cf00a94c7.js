512095d8c4d3c5265135153e2d18805df1067ede
SamPrewitt <prewittsr@appstate.edu>
2022-04-11 00:52:18 -0400

    Updates

---
+++ precache-manifest.d6e1bd5cd5774c2bb5b20e2cf00a94c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d6b8a16e1f5f6eb3c59",
+    "url": "/Covid-Today-WebApp/static/css/main.189f8315.chunk.css"
+  },
+  {
+    "revision": "3d6b8a16e1f5f6eb3c59",
+    "url": "/Covid-Today-WebApp/static/js/main.0b08b5d6.chunk.js"
+  },
+  {
+    "revision": "705fdd67b87de4bbce74",
+    "url": "/Covid-Today-WebApp/static/js/runtime~main.2499f985.js"
+  },
+  {
+    "revision": "2529eb737d97efc51092",
+    "url": "/Covid-Today-WebApp/static/js/2.398f6ee2.chunk.js"
+  },
+  {
+    "revision": "d1b1c15550733d4c89ea3d2b1bb1ea5c",
+    "url": "/Covid-Today-WebApp/static/media/covidtoday_logo.d1b1c155.jpg"
+  },
+  {
+    "revision": "aceffbe5605fdc371c9c6f344ab4be56",
+    "url": "/Covid-Today-WebApp/index.html"
+  }
+];

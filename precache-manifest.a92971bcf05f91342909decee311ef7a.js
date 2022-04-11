4b7585c3a743a0a09375e86ead19edc208ad0374
ChaseAlll <chase@chaseallison.com>
2022-04-11 17:01:38 -0400

    Updates

---
+++ precache-manifest.a92971bcf05f91342909decee311ef7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5d26e177a082973a0e1",
+    "url": "/Covid-Today-WebApp/static/css/main.d9b459bf.chunk.css"
+  },
+  {
+    "revision": "a5d26e177a082973a0e1",
+    "url": "/Covid-Today-WebApp/static/js/main.f3bc950d.chunk.js"
+  },
+  {
+    "revision": "705fdd67b87de4bbce74",
+    "url": "/Covid-Today-WebApp/static/js/runtime~main.2499f985.js"
+  },
+  {
+    "revision": "84f4c78a6dd2e9b31b77",
+    "url": "/Covid-Today-WebApp/static/js/2.e7369e11.chunk.js"
+  },
+  {
+    "revision": "d1b1c15550733d4c89ea3d2b1bb1ea5c",
+    "url": "/Covid-Today-WebApp/static/media/covidtoday_logo.d1b1c155.jpg"
+  },
+  {
+    "revision": "0bdd1a791fd277c3d69637ef08ad78f9",
+    "url": "/Covid-Today-WebApp/index.html"
+  }
+];

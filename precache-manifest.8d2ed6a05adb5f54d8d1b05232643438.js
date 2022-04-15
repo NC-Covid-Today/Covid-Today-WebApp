4f4224e4540921156680ea2a34f693df0d8500f8
ChaseAlll <chase@chaseallison.com>
2022-04-15 12:34:08 -0400

    Updates

---
+++ precache-manifest.8d2ed6a05adb5f54d8d1b05232643438.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5caeebb78400c666064",
+    "url": "/Covid-Today-WebApp/static/css/main.ebd70eb4.chunk.css"
+  },
+  {
+    "revision": "c5caeebb78400c666064",
+    "url": "/Covid-Today-WebApp/static/js/main.1206b77b.chunk.js"
+  },
+  {
+    "revision": "705fdd67b87de4bbce74",
+    "url": "/Covid-Today-WebApp/static/js/runtime~main.2499f985.js"
+  },
+  {
+    "revision": "f0e86fc9b63051d2c4c3",
+    "url": "/Covid-Today-WebApp/static/js/2.6057951b.chunk.js"
+  },
+  {
+    "revision": "d1b1c15550733d4c89ea3d2b1bb1ea5c",
+    "url": "/Covid-Today-WebApp/static/media/covidtoday_logo.d1b1c155.jpg"
+  },
+  {
+    "revision": "4b2644d155e7fca96cbf761387e6e6e3",
+    "url": "/Covid-Today-WebApp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Covid-Today-WebApp/precache-manifest.39117a5a4db3ba79bf3bd4765842a9bc.js"
+  "/Covid-Today-WebApp/precache-manifest.8d2ed6a05adb5f54d8d1b05232643438.js"
 );
 
 workbox.clientsClaim();

645c9db78b4c880672f247f066817e1c15ee1ab0
ChaseAlll <chase@chaseallison.com>
2022-05-01 19:47:00 -0400

    Changed about page spacing

--- src/pages/about.js
+++ src/pages/about.js
@@ -15,13 +15,16 @@ const About = () => {
 
       <center>
       <h2 class="about">Why use our site?</h2>
+      </center>
+      
+
       <ul class="about">
         <li>Get local information fast</li>
         <li>Stay up to date on cases around the world</li>
         <li>View the latest Watagua COVID-19 Situation Update</li>
         <li>Relevant COVID-19 news from around the world</li>
       </ul>
-      </center>
+      
 
       <p class="about">
         Feel free to visit the <a href="https://covid19.ncdhhs.gov/dashboard" target="_blank"> NC Covid Dashboard</a> as well!
